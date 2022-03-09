b3a186824fd9f0bf582b03f577bb4d51fb2d78d1
Aselkrion <aselkrion@yandex.ru>
2022-03-09 14:31:01 +0300

    Project_version_0.2 (CoffeeMachine_version_3.0)

--- CoffeMachine/src/Main.java
+++ CoffeMachine/src/Main.java
@@ -2,20 +2,40 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int coffeeAmount = 2330;
+        int coffeeAmount = 100;
         int milkAmount = 100;
         int cappucinoMilkRequired = 60;
         int cappucinoCoffeeRequired = 50;
         int skimmedMilkAmount = 500;
+        int lowLactoseMilkAmount = 1000;
 
-        boolean isBlocked = false;
+        boolean milkIsEnough = skimmedMilkAmount >= cappucinoMilkRequired ||
+                milkAmount >= cappucinoMilkRequired ||
+                lowLactoseMilkAmount >= cappucinoMilkRequired;
 
-        if (!isBlocked && milkAmount >= cappucinoMilkRequired &&
-                coffeeAmount >= cappucinoCoffeeRequired ||
-                skimmedMilkAmount >= cappucinoMilkRequired) {
-            System.out.println("готовим кофе");
-        } else {
-            System.out.println("Кофе-машина заблокирована");
+        boolean coffeeIsEnough = coffeeAmount >= cappucinoCoffeeRequired;
+
+        boolean isBlocked = true;
+
+        boolean hasErrors = false;
+
+        if (isBlocked) {
+            System.out.println("Кофе машина заблокирована");
+            hasErrors = true;
+        }
+
+        if (!coffeeIsEnough) {
+            System.out.println("Кофе недостаточно");
+            hasErrors = true;
+        }
+
+        if (!milkIsEnough) {
+            System.out.println("Молока недостаточно");
+            hasErrors = true;
+        }
+
+        if (!hasErrors) {
+            System.out.println("Готовим кофе");
         }
 
     }
